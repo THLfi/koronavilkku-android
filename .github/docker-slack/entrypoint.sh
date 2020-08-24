165b3d1691210d3ea2fd181ae81d3b617d630fc1
Alpertti Tirronen <alpertti.tirronen@solita.fi>
2020-08-24 17:08:58 +0300

    ignore linter owasp warnings + use reponame as topic for slack messages

--- .github/docker-slack/entrypoint.sh
+++ .github/docker-slack/entrypoint.sh
@@ -6,7 +6,7 @@ JOB_STATUS=$(echo "$INPUT_STATUS" | tr '[:lower:]' '[:upper:]')
 WORKFLOW_RUN_PATH="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
 
 PAYLOAD=$(jq -n \
-  --arg text "*$INPUT_TEXT $JOB_STATUS*." \
+  --arg text "*$GITHUB_REPOSITORY: $INPUT_TEXT $JOB_STATUS*." \
   --arg wr_path "$WORKFLOW_RUN_PATH" \
   '{blocks:[{type:"section",text:{type:"mrkdwn",text: $text}},{type: "divider"},{type:"actions",elements:[{type:"button",text:{type:"plain_text",text:"View run",emoji:true},url:$wr_path}]}]}')
 
