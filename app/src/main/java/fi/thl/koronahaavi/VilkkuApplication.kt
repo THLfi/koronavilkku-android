cc52214d6f80e3bf4350c87e8d5373f991f56d96
Jussi Heikkola <jussi.heikkola@solita.fi>
2020-08-24 14:46:54 +0300

    schedule workers also during app startup, to make sure scheduling is in place if app is woken up after a force-stop

--- app/src/main/java/fi/thl/koronahaavi/VilkkuApplication.kt
+++ app/src/main/java/fi/thl/koronahaavi/VilkkuApplication.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.*
 import dagger.hilt.android.HiltAndroidApp
+import fi.thl.koronahaavi.data.AppStateRepository
+import fi.thl.koronahaavi.service.WorkDispatcher
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 import javax.inject.Inject
@@ -13,6 +15,8 @@ import javax.inject.Inject
 class VilkkuApplication : Application(), Configuration.Provider {
 
     @Inject lateinit var workerFactory: HiltWorkerFactory
+    @Inject lateinit var workDispatcher: WorkDispatcher
+    @Inject lateinit var appStateRepository: AppStateRepository
 
     // this is required to inject workers
     override fun getWorkManagerConfiguration() =
@@ -26,5 +30,11 @@ class VilkkuApplication : Application(), Configuration.Provider {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) Timber.plant(DebugTree())
+
+        // This is an additional check to make sure workers are scheduled in case
+        // app was force-stopped from background and woken up by EN api service.
+        if (appStateRepository.isOnboardingComplete()) {
+            workDispatcher.scheduleWorkers()
+        }
     }
 }
