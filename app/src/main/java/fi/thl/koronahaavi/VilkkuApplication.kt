aaf32757ea2ef2ecc9ebae3d26b5947e442e9d84
Jussi Heikkola <62838201+solita-jussihei@users.noreply.github.com>
2020-08-25 13:44:57 +0300

    Work manager scheduling changes to support app process restart by EN api (#3)
    * use manual initialization for work manager, and schedule workers, at app startup to support automatic process restart by EN api
    * use relative paths in backend endpoints to allow for path in base url

--- app/src/main/java/fi/thl/koronahaavi/VilkkuApplication.kt
+++ app/src/main/java/fi/thl/koronahaavi/VilkkuApplication.kt
@@ -5,26 +5,38 @@ import android.util.Log
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.*
 import dagger.hilt.android.HiltAndroidApp
+import fi.thl.koronahaavi.data.AppStateRepository
+import fi.thl.koronahaavi.service.WorkDispatcher
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 import javax.inject.Inject
 
 @HiltAndroidApp
-class VilkkuApplication : Application(), Configuration.Provider {
+class VilkkuApplication : Application() {
 
     @Inject lateinit var workerFactory: HiltWorkerFactory
+    @Inject lateinit var workDispatcher: WorkDispatcher
+    @Inject lateinit var appStateRepository: AppStateRepository
 
-    // this is required to inject workers
-    override fun getWorkManagerConfiguration() =
-        Configuration.Builder().apply {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(DebugTree())
+
+        // this is required to inject workers
+        val config = Configuration.Builder().apply {
             setWorkerFactory(workerFactory)
             if (BuildConfig.DEBUG) {
                 setMinimumLoggingLevel(Log.DEBUG)
             }
         }.build()
 
-    override fun onCreate() {
-        super.onCreate()
-        if (BuildConfig.DEBUG) Timber.plant(DebugTree())
+        // manual initialization required by EN api service wake up service
+        WorkManager.initialize(this, config)
+
+        // This is an additional check to make sure workers are scheduled in case
+        // app was force-stopped from background and woken up by EN api service.
+        if (appStateRepository.isOnboardingComplete()) {
+            workDispatcher.scheduleWorkers()
+        }
     }
 }
