1d8de0e0e7c0bd178a11d3ebd086dd382b25bdd4
Alpertti Tirronen <alpertti.tirronen@solita.fi>
2020-08-27 15:08:02 +0300

    use env instead of parameter

--- .github/docker-owasp-dependency-check-android/entrypoint.sh
+++ .github/docker-owasp-dependency-check-android/entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 
 ls -Rla /home
 
-sh -c "./gradlew --no-daemon -g=$INPUT_GRADLE_HOME dependencyCheckAnalyze"
+sh -c "./gradlew --no-daemon -g=$GRADLE_HOME dependencyCheckAnalyze"
 
 ls -Rla /home
