251e3b254a3f9b98c6a6414c77492d540c105afd
Jussi Heikkola <jussi.heikkola@solita.fi>
2020-10-07 09:59:55 +0300

    add missing file

---
+++ app/src/liveGoogle/java/fi/thl/koronahaavi/di/ExposureNotificationModule.kt
@@ -0,0 +1,22 @@
+package fi.thl.koronahaavi.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import fi.thl.koronahaavi.service.ExposureNotificationService
+import fi.thl.koronahaavi.service.GoogleExposureNotificationService
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object ExposureNotificationModule {
+
+    @Singleton
+    @Provides
+    fun provideExposureNotificationService(@ApplicationContext context: Context): ExposureNotificationService {
+        return GoogleExposureNotificationService(context)
+    }
+}
