7196cc9c8ee85af6480c2d4ede954137bfbbfce7
Jussi Heikkola <62838201+solita-jussihei@users.noreply.github.com>
2020-10-22 14:01:35 +0300

    Reschedule update worker on app create (#54)
    When exposure update and cleanup workers have been pendinga long time, reschedule them at app process startup in an attempt to trigger execution

--- app/src/main/java/fi/thl/koronahaavi/service/WorkDispatcher.kt
+++ app/src/main/java/fi/thl/koronahaavi/service/WorkDispatcher.kt
@@ -3,16 +3,19 @@ package fi.thl.koronahaavi.service
 import android.content.Context
 import androidx.work.*
 import dagger.hilt.android.qualifiers.ApplicationContext
+import fi.thl.koronahaavi.data.AppStateRepository
 import fi.thl.koronahaavi.data.SettingsRepository
 import fi.thl.koronahaavi.exposure.ClearExpiredExposuresWorker
 import fi.thl.koronahaavi.exposure.MunicipalityUpdateWorker
+import java.time.ZonedDateTime
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class WorkDispatcher @Inject constructor(
     @ApplicationContext private val context: Context,
-    private val settingsRepository: SettingsRepository
+    private val settingsRepository: SettingsRepository,
+    private val appStateRepository: AppStateRepository
 )  {
     fun cancelWorkersAfterLock() {
         with (WorkManager.getInstance(context)) {
@@ -24,10 +27,26 @@ class WorkDispatcher @Inject constructor(
         }
     }
 
-    fun scheduleWorkers() {
-        DiagnosisKeyUpdateWorker.schedule(context, settingsRepository.appConfiguration)
+    fun scheduleWorkers(reconfigureStale: Boolean = false) {
+        // Attempting to trigger worker to execute by reconfiguring at app startup
+        // This seems to be required on some android models, like Xiaomi, to execute worker
+        // when Google services periodically restart the app process using WakeUpService
+
+        val reconfigure = reconfigureStale && isLastExposureCheckOld()
+        DiagnosisKeyUpdateWorker.schedule(context, settingsRepository.appConfiguration, reconfigure)
+        ClearExpiredExposuresWorker.schedule(context, reconfigure)
+
         MunicipalityUpdateWorker.schedule(context)
         DiagnosisKeySendTrafficCoverWorker.schedule(context)
-        ClearExpiredExposuresWorker.schedule(context)
+    }
+
+    private fun isLastExposureCheckOld(): Boolean {
+        // last check is considered old if it's been longer than two update intervals, because
+        // work manager execution is not exact, and we do not want to always update on process start
+        // since that would give the impression that updates never work on the background
+        val intervalMinutes = settingsRepository.appConfiguration.pollingIntervalMinutes
+        val limit = ZonedDateTime.now().minusMinutes(2 * intervalMinutes)
+
+        return appStateRepository.getLastExposureCheckTime()?.isBefore(limit) != false
     }
 }
