4abd27b02a1051d0cb4f4416f6c05807004a7b8a
Alpertti Tirronen <alpertti.tirronen@solita.fi>
2020-12-05 10:38:47 +0200

    allow new versions of suppressions file, don't scan lint, updgraded owasp dependency plugin

--- .github/docker-owasp-dependency-check-android/entrypoint.sh
+++ .github/docker-owasp-dependency-check-android/entrypoint.sh
@@ -3,4 +3,4 @@
 set -e
 
 
-sh -c "./gradlew --no-daemon -g=$GRADLE_HOME dependencyCheckAnalyze"
+sh -c "./gradlew --no-daemon -g=$GRADLE_HOME dependencyCheckAnalyze -PowaspSuppressionFile=$(ls owasp_suppressions_* | tail -1)"
