450c9ab0f6e9c495ff1dd9ad3bad877ab153d16f
Jussi Heikkola <jussi.heikkola@solita.fi>
2021-03-01 13:52:49 +0200

    Update notification channel name when locale changes

--- app/src/main/java/fi/thl/koronahaavi/language/LocaleChangedReceiver.kt
+++ app/src/main/java/fi/thl/koronahaavi/language/LocaleChangedReceiver.kt
@@ -3,13 +3,20 @@ package fi.thl.koronahaavi.language
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import dagger.hilt.android.AndroidEntryPoint
+import fi.thl.koronahaavi.service.NotificationService
 import timber.log.Timber
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class LocaleChangedReceiver : BroadcastReceiver() {
+    @Inject
+    lateinit var notificationService: NotificationService
+
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_LOCALE_CHANGED) {
-            Timber.d("Locale changed")
-            // todo inject notification service and initialize
+            // this updates notification channel name to match current locale
+            notificationService.initialize()
         }
     }
 }
